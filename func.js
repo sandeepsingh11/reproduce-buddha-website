acb5d5a90b887b69e7c98a6f46cc4d4aadc5007a
Sandeep Singh <serperior2@gmail.com>
2018-12-22 23:11:49 -0500

    add carousel functionality

--- func.js
+++ func.js
@@ -12,4 +12,35 @@ $(document).ready(function() {
 			$('#nav').css('position', 'absolute');
 		}
 	});
+
+	$('#heroCarousel').slick({
+		dots: true,
+		arrows: false,
+		infinite: true,
+		adaptiveHeight: true,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		speed: 1000,
+		fade: true,
+		cssEase: 'linear'
+	});
+
+	$('.fade').slick({
+		dots: false,
+		infinite: true,
+		adaptiveHeight: true,
+		speed: 500,
+		fade: true,
+		cssEase: 'linear'
+	});
+
+	$('#photos').slick({
+		dots: false,
+		infinite: true,
+		adaptiveHeight: true,
+		speed: 500,
+		rows: 2,
+		slidesPerRow: 5,
+		appendArrows: $('#appendArrows')
+	});
 });
